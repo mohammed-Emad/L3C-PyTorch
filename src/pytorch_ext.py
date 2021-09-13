d6d0d371ac448ff530a73739386c334e5c24cb09
mohammed-Emad <mohammed.imad38@yahoo.com>
2021-09-13 14:00:59 +0200

    Update pytorch_ext.py

--- src/pytorch_ext.py
+++ src/pytorch_ext.py
@@ -42,13 +42,15 @@ CUDA_AVAILABLE = torch.cuda.is_available()
 # if IGNORE_CUDA:
 #     print('*** IGNORE_CUDA=1')
 
-DEVICE = torch.device("cuda:0" if CUDA_AVAILABLE else "cpu")
+#DEVICE = torch.device("cuda:0" if CUDA_AVAILABLE else "cpu")
+
 
 
 # This has an effect to all parts reading pytorch_ext.DEVICE *after* it has been set.
 def set_device(cuda_available):
     global DEVICE
-    DEVICE = torch.device("cuda:0" if cuda_available else "cpu")
+    #DEVICE = torch.device("cuda:0" if cuda_available else "cpu")
+    DEVICE = torch.device("cpu")
 
 
 # Conv -------------------------------------------------------------------------
